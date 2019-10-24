68a816b5bd51bc9546da27027452cb39eec5ee92
tameem6 <tameem6ansar@gmail.com>
2019-10-24 15:50:59 +0530

    Validation for all fields

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const joi = require('joi');
 const User = require('../Models/User');
 
+const schema = joi.object().keys({
+    username: joi.string().required().min(5),
+    name: joi.string().required().min(6),
+    password: joi.string().required().min(6)
+});
+
 router.post('/register', async (req,res) => {
+    const {error} = joi.validate(req.body, schema);
+    if(error) {
+        return res.status(400).send(error.details[0].message);
+    }
     const user = new User({
         username: req.body.username,
         name: req.body.name,
