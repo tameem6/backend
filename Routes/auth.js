c8ea2f6e45a2cba9a5abe2090b161b500447887a
tameem6 <tameem6ansar@gmail.com>
2019-10-24 17:28:25 +0530

    Login function added

--- Routes/auth.js
+++ Routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../Models/User');
 const {registerValidation, loginValidation} = require('../validation');
-
+const bcrypt = require('bcryptjs');
 
 router.post('/register', async (req,res) => {
     const {error} = registerValidation(req.body);
@@ -13,22 +13,37 @@ router.post('/register', async (req,res) => {
     if(userExist) {
         return res.status(400).send("Username already taken");
     }
+    //Hash passwprds
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
     const user = new User({
         username: req.body.username,
         name: req.body.name,
-        password: req.body.password
+        password: hashPassword
     });
     try{
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.status(200).send({username: user.username});
     } catch(err) {
         res.status(400).send(err);
     }
 });
-
-// router.post('/login', (req,res) => {
-
-// })
+//Login
+router.post('/login', async (req,res) => {
+    const {error} = loginValidation(req.body);
+    if(error) {
+        return res.status(400).send(error.details[0].message);
+    }
+    const user = await User.findOne({username: req.body.username});
+    if(!user) {
+        return res.status(400).send("Username not found");
+    }
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass) {
+        return res.status(400).send("Invalid Passowrd");
+    }
+    res.status(200).send("Logged In");
+})
 
 
 module.exports = router;
